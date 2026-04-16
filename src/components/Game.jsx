19ad0671f7af99bdbfc113a9eb04f9ae6a8a8625
ggvictor <victorrodriguessilva2@gmail.com>
2026-04-16 18:07:03 -0300

    first commit

---
+++ src/components/Game.jsx
@@ -0,0 +1,52 @@
+import './game.css'
+import { useState, useRef } from 'react'
+
+const Game = ({verifyLetter, pickedWord, pickedCategory, letters,guessedLetters,wrongLetters,guesses,score}) => {
+  const [letter, Setletter] = useState("")
+  const letterInputRef = useRef(null)
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+
+    verifyLetter(letter)
+    Setletter("")
+
+    letterInputRef.current.focus();
+  }
+  return (
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: {score}</span>
+      </p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>{pickedCategory}</span>
+      </h3>
+      <p>Você ainda tem {guesses} tentativa(s).</p>
+      <div className="wordContainer">
+        {letters.map((letter, i) => (
+          guessedLetters.includes(letter) ? (
+            <span className = "letter" key = {i}>{letter}</span>
+          ) : (
+            <span className="blankSquare" key={i}></span>
+          )
+        ))}
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name='letter' maxLength="1" required onChange={(e) => Setletter(e.target.value)} value = {letter} ref = {letterInputRef}/>    
+          <button>Jogar!</button> 
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        {wrongLetters.map((letter, i) =>(
+          <span key = {i}>{letter}, </span>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Game
